55aac30528c3b63a986c366fc96792d689e678ad
felipe toledo <felipetoledo07@gmail.com>
2024-02-01 10:44:44 -0300

    palindrome_number.py directory rename and roman_to_integer.py problem setup

---
+++ 13_roman_to_integer/roman_to_integer.py
@@ -0,0 +1,10 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        return 1
+
+
+solution = Solution()
+
+print('Case 1: ', solution.romanToInt("III"))
+print('Case 1: ', solution.romanToInt("LVIII"))
+print('Case 1: ', solution.romanToInt("MCMXCIV"))
