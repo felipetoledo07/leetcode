dfdbb6937ca799e89c263b9aadc0441b7c24aadf
felipe toledo <felipetoledo07@gmail.com>
2024-02-01 21:35:08 -0300

    palindrome_number.py directory rename and roman_to_integer.py solution

--- 13_roman_to_integer/roman_to_integer.py
+++ 13_roman_to_integer/roman_to_integer.py
@@ -1,6 +1,21 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        return 1
+        roman_numbers = {
+            'M': 1000,
+            'D': 500,
+            'C': 100,
+            'L': 50,
+            'X': 10,
+            'V': 5,
+            'I': 1,
+        }
+        return_number = 0
+        for i in range(len(s)):
+            if i != len(s) - 1 and roman_numbers[s[i]] < roman_numbers[s[i + 1]]:
+                return_number -= roman_numbers[s[i]]
+            else:
+                return_number += roman_numbers[s[i]]
+        return return_number
 
 
 solution = Solution()
